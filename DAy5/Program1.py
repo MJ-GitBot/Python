0cb4afe3cc50e7af4aeddd3c54dd769bba740aaa
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2026-07-29 08:57:11 +0530

    Day 5 notes

--- DAy5/Program1.py
+++ DAy5/Program1.py
@@ -1,1 +1,9 @@
-print("Day5 ")
+uname = input("Enter Name: ")
+upass = input("Enter Password: ")
+      #Suresh !="Thanesh"   and upass!="Thanesh@123"
+while uname != "Thanesh" and upass != "Thanesh@123":
+    print("Login Failed! Please try again.")
+    uname = input("Enter Name: ")
+    upass = input("Enter Password: ")
+
+print("Login Successful!")
